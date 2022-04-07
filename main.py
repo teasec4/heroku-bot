d1fab663fd41282d368e9765a2e6d8d089bfcb42
teasec4 <85108614+teasec4@users.noreply.github.com>
2022-04-07 16:11:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import logging
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+TOKEN = '5256719525:AAFR0Zouz-j5R-OWLAqES7M2ZVc3QHYYn0o'
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Im a bot, please talk to me')
+
+
+start_handler = CommandHandler('start', start)
+
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
